16e831bb13df96afde2a85ba9c662441b90c8f66
Parth <parth.mehrotra.cs@gmail.com>
2017-07-18 17:10:22 +0000

    deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:cwchien/gradle
 sudo apt update
-sudo apt install nodejs npm default-jdk default-jre gradle tmux
+sudo apt install nodejs-legacy nodejs npm default-jdk default-jre gradle tmux
 
 tmux set-option remain-on-exit on
 tmux split-window 'cd frontend && npm start' 
